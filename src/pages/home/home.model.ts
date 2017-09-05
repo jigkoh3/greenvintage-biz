e5196f768e697de100fbf58080432165bac51251
mob35 <toonelicious@gmail.com>
2017-09-05 18:54:13 +0700

    chart

--- src/pages/home/home.model.ts
+++ src/pages/home/home.model.ts
@@ -1,16 +1,36 @@
+// export class HomeModel {
+//     images: Array<String>;
+//     popularproducts: Array<ProductItemModel>;
+//     popularshops: Array<ShopItemModel>;
+//   }
+
+//   export class ProductItemModel {
+//     name: String;
+//     image: String;
+//     unitprice: Number;
+//   }
+//   export class ShopItemModel {
+//     name: String;
+//     image: String;
+//   }
+
+
 export class HomeModel {
-    images: Array<String>;
-    popularproducts: Array<ProductItemModel>;
-    popularshops: Array<ShopItemModel>;
-  }
-  
-  export class ProductItemModel {
-    name: String;
-    image: String;
-    unitprice: Number;
-  }
-  export class ShopItemModel {
-    name: String;
-    image: String;
-  }
-  
+  waiting: Array<HomeItemModel>;
+  accept: Array<HomeItemModel>;
+  unreceive: Array<HomeItemModel>;
+}
+
+export class HomeItemModel {
+  docno: String;
+  name: String;
+  image: String;
+  price: Number;
+  normalprice: Number;
+  discount: Number;
+  discounttype: String;
+  currency: String;
+  qty: Number;
+  status: String;
+  description: string;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Chart } from 'chart.js';
+import { HomeService } from "./home.service";
+import { HomeModel } from "../home/home.model";
 
 /**
  * Generated class for the HomePage page.
@@ -13,12 +16,91 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home.html',
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  homeData: HomeModel = new HomeModel();
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+  @ViewChild('lineCanvas') lineCanvas;
+  doughnutChart: any;
+  lineChart: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public homeService: HomeService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+    this.getHome();
+
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+
+      type: 'doughnut',
+      data: {
+        labels: ["waiting", "accept", "unreceive", "receive"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#FF6384"
+          ]
+        }]
+      }, legend: {
+        align: "right",
+        layout: "vertical",
+        // verticalAlign: 'top',
+        x: 40,
+        y: 0
+      }
+
+    });
+
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+      type: 'line',
+      data: {
+        labels: ["waiting", "accept", "unreceive", "receive"],
+        datasets: [
+          {
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81],
+            spanGaps: false,
+          }
+        ]
+      }
+
+    });
+
+
+  }
+
+  getHome() {
+    this.homeService.getData().then(data => {
+      this.homeData = data;
+      console.log(this.homeData);
+    })
+
   }
 
 }
