893a596b001e93ea51659eab7d372a2458860a33
moomam6934 <destinationpainbm@gmail.com>
2017-09-05 15:58:33 +0700

    ceate  file productlist

---
+++ src/app/app.model.ts
@@ -0,0 +1,11 @@
+export class ProductItemModel {
+    name: String;
+    image: String;
+    price: Number;
+    normalprice: Number;
+    discount: Number;
+    discounttype: String;
+    currency: String;
+    rate: Number;
+    description: string;
+  }
