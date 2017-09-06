3383e2dd360bbc4125b0cedd5263e753ad79aac2
Sirintra Wannakheaw <sirintrawannakeaw@gmail.com>
2017-09-06 16:11:08 +0700

    Update order call service

--- src/pages/order/order.service.ts
+++ src/pages/order/order.service.ts
@@ -18,7 +18,7 @@ export class OrderServiceProvider {
   }
 
   getData(){
-    return this.http.get('../../assets/example_data/order.json')
+    return this.http.get('https://greenvintage.herokuapp.com/api/orderlistbyshops')
     .toPromise()
     .then( resp => resp.json())
     .catch(err => Promise.reject(err.message || err));
