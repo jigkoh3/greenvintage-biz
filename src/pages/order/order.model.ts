86316655f9e7685f9f6ecb25239585903ed66662
Sirintra Wannakheaw <sirintrawannakeaw@gmail.com>
2017-09-06 16:13:15 +0700

    Update list order

--- src/pages/order/order.model.ts
+++ src/pages/order/order.model.ts
@@ -1,24 +1,3 @@
-// export class OrderModel {
-//     waiting: Array<OrderItemModel>;
-//     accept: Array<OrderItemModel>;
-//     unreceive: Array<OrderItemModel>;
-// }
-
-// export class OrderItemModel {
-//     docno: String;
-//     name: String;
-//     image: String;
-//     price: Number;
-//     normalprice: Number;
-//     discount: Number;
-//     discounttype: String;
-//     currency: String;
-//     qty: Number;
-//     status: String;
-//     description: string;
-// }
-
-
 export class OrderModel {
     _id: string;
     created: string;
